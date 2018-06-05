4089eecda1f2c19ac1b58165b52922401c5c69fe
Kohen Chia <kohenchia@gmail.com>
2018-06-05 16:58:22 -0700

    Basic streaming is now working

--- web/src/App.js
+++ web/src/App.js
@@ -2,40 +2,34 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      frame: null
+    constructor(props) {
+        super(props);
+        this.state = {
+            frame: null
+        }
+        this.ws = new WebSocket('ws://172.16.20.70/eyeofgollum/video/stream');
+        this.ws.binaryType = 'arraybuffer';
+        this.ws.onopen = (event => this.ws.send('.'));
+        this.ws.onmessage = (event => this.handleData(event.data));
     }
-  }
-  componentDidMount() {
-    fetch('/video/frame')
-      .then(ret => ret.json())
-      .then((ret) => {
+    handleData(data) {
+        let base64String = btoa(String.fromCharCode(...new Uint8Array(data)));
         this.setState({
-          frame: ret.frame
+            frame: base64String
         })
-      });
-  }
-  handleData(data) {
-    console.log(typeof(data));
-    console.log(data.length);
-    // TODO: Serialize data to base-64
-    this.setState({
-      frame: null
-    })
-  }
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">AI2 Incubator Camera</h1>
-        </header>
-        <img src={`data:image/jpeg;base64,${this.state.frame}`}
-             alt='AI2 Incubator Live Feed'/>
-      </div>
-    );
-  }
+        this.ws.send('.');
+    }
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <h1 className="App-title">AI2 Incubator Camera</h1>
+                </header>
+                <img src={`data:image/jpeg;base64,${this.state.frame}`}
+                    alt='AI2 Incubator Live Feed' />
+            </div>
+        );
+    }
 }
 
 export default App;
